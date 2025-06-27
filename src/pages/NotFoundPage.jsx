5061cb9aa8f8ee7d41e74e77338d4bb64c6f6a56
Nasar Uddin <dearnasaruddin@gmail.com>
2025-06-27 22:41:37 +0600

    not found image path fixed

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
         <div className='h-screen w-full flex flex-col gap-12 items-center justify-center bg-primary'>
             <h2 className='text-3xl text-[#3F3D56]'>NOT_FOUND</h2>
             <div className='w-1/5'>
-                <img src="images/notFoundImg.svg" alt="notFoundImg.svg" />
+                <img src="/images/notFoundImg.svg" alt="notFoundImg.svg" />
             </div>
             <Link to={'/'} className='bg-secondary py-2 px-4 rounded-lg cursor-pointer text-white'>Back to Home</Link>
         </div>
